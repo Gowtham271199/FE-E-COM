f5640c69bde7988a956c84058a2e57e0c2a2e774
Gowtham271199 <gothammelo27@gmail.com>
2024-05-18 21:29:52 +0530

    first commit

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -1,34 +1,27 @@
-import React from 'react'
-import './Breadcrum.css'
-import arrow_icon from '../Assets/breadcrum_arrow.png'
+import React from 'react';
+import './Breadcrum.css';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
 
- export const Breadcrum = (props) => {
-  
-    const {product}=props
-    console.log(product)
-    // return (
-  //     <div className='breadcrum'>
-  //       HOME <img src={arrow_icon} alt='' /> SHOP{' '}
-  //       {product && product.category && (
-  //         <>
-  //           <img src={arrow_icon} alt='' />
-  //           {product.category}
-  //         </>
-  //       )}
-  //       {product && product.name && (
-  //         <>
-  //           <img src={arrow_icon} alt='' />
-  //           {product.name}
-  //         </>
-  //       )}
-  //     </div>
-  //   );
-  // };
+export const Breadcrum = (props) => {
+  const { product } = props;
+
   return (
     <div className='breadcrum'>
-        
-        HOME <img src={arrow_icon} alt='arrow'/> SHOP <img src={arrow_icon} alt="arrow"/>{product.category}<img src={arrow_icon} alt="arrow" />{product.name} 
-        </div>
-  )
-}
-export default Breadcrum
+      HOME <img src={arrow_icon} alt='arrow' /> SHOP{' '}
+      {product && product.category && (
+        <>
+          <img src={arrow_icon} alt='arrow' />
+          {product.category}
+        </>
+      )}
+      {product && product.name && (
+        <>
+          <img src={arrow_icon} alt='arrow' />
+          {product.name}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Breadcrum;
